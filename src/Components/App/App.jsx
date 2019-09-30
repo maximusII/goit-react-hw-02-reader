f5479bacf3810223a45661011a86989a74342bc1
Maksym Osadchuk <mosadchuk7@gmail.com>
2019-09-30 23:31:46 +0300

    Create App component

---
+++ src/Components/App/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./App.module.css";
+
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default App;
